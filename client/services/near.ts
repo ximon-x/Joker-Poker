d3172aebc8eac2db3c83c3e3ead0bacced4dd376
Simon Samuel <salviensky@gmail.com>
2022-11-17 17:30:35 +0100

    Setting up the modal for the selector

--- client/services/near.ts
+++ client/services/near.ts
@@ -1,9 +1,26 @@
+import { setupWalletSelector } from "@near-wallet-selector/core";
+import { setupMathWallet } from "@near-wallet-selector/math-wallet";
+import { setupModal } from "@near-wallet-selector/modal-ui";
+import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupNearWallet } from "@near-wallet-selector/near-wallet";
 import { connect } from "near-api-js";
 import config from "../config";
+import { Network } from "../types";
+
+const network: Network = "testnet";
 
 async function initContract() {
-  const nearEnv = config.environment("testnet");
+  const nearEnv = config.environment(network);
   const near = await connect(nearEnv);
 }
 
-// Wallet Setup
+const selector = await setupWalletSelector({
+  network: network,
+  modules: [setupMathWallet(), setupNearWallet(), setupMyNearWallet()],
+});
+
+export const modal = setupModal(selector, {
+  contractId: config.contractName!,
+});
+
+modal.show();
