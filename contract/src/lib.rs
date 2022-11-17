4233e510a44f6a0d4c3384adb5c72d08c7535f5e
Simon Samuel <salviensky@gmail.com>
2022-11-17 16:46:53 +0100

    Placed business logic comments in lib.rs

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,6 +1,9 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::near_bindgen;
 
+// Player Mapping {address -> {rank, points}}
+// Card enums RANK, VALUE
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct JokerPoker {
@@ -13,9 +16,24 @@ impl JokerPoker {
     pub fn init() -> Self {
         Self { card_value: 0 }
     }
+
+    // function for generating random card
+
+    // higher_lower function
+
+    // joker_poker function
+
+    // function for paying player
 }
 
 // #[cfg(test)]
 // mod tests {
 //     use super::*;
+//      test for randomness
+
+//      test for higher_lower
+
+//      test for joker_poker
+
+//      test for transfer NEAR
 // }
