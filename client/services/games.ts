afb22901d36fe5d930d4bcb8986de85cf7f7bdd5
Simon Samuel <salviensky@gmail.com>
2022-11-17 18:13:25 +0100

    Added view and change contract methods

---
+++ client/services/games.ts
@@ -0,0 +1,7 @@
+import { parseNearAmount } from "near-api-js/lib/utils/format";
+
+export function higherOrLower() {}
+
+export function jokerPoker({}) {
+  return window.contract.joker_poker({});
+}
