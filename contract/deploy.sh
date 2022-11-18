ae73eee99e4c36183e41fdf8245306bb48c01502
Simon Samuel <salviensky@gmail.com>
2022-11-18 13:40:21 +0100

    Modified build scripts

--- contract/deploy.sh
+++ contract/deploy.sh
@@ -3,12 +3,20 @@
 ./build.sh
 
 if [ $? -ne 0 ]; then 
-    echo "=== Error Building Contract ==="
+    echo "\n=== Error Building Contract ==="
     exit 1
 fi
 
-echo "=== Deploying contract ==="
+echo "\n=== Deploying contract ==="
 
+echo "\n== Deleting previous contract =="
 near delete joker_poker.ximon.testnet ximon.testnet 
+
+echo "\n== Creating new Account =="
 near create-account joker_poker.ximon.testnet --masterAccount ximon.testnet --initialBalance 5
-near deploy --accountId=joker_poker.ximon.testnet --wasmFile=target/wasm32-unknown-unknown/release/contract
+
+echo "\n== Deploying wasm file =="
+near deploy --accountId=joker_poker.ximon.testnet --wasmFile=target/wasm32-unknown-unknown/release/contract.wasm
+
+echo "\n== Initializing the Deployed contract =="
+near call joker_poker.ximon.testnet init --accountId=ximon.testnet
