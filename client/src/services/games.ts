b09cc96f2a2040d33d8d0321ec9b0e0ffc878eea
Simon Samuel <salviensky@gmail.com>
2022-11-18 10:41:56 +0100

    Reverting all client changes to complete contracts

--- client/src/services/games.ts
+++
@@ -1,7 +0,0 @@
-import { parseNearAmount } from "near-api-js/lib/utils/format";
-
-export function higherOrLower() {}
-
-export function jokerPoker({}) {
-  return window.contract.joker_poker({});
-}
