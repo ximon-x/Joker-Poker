5b57b0d32f1189bfdc5e370c6c8a29c2d46489a7
Simon Samuel <salviensky@gmail.com>
2022-11-20 08:04:30 +0100

    Saved frequents commands

---
+++ contract/commands.sh
@@ -0,0 +1,11 @@
+# Registering a player
+near call joker_poker.ximon.testnet register_player --accountId=ximon.testnet
+
+# Getting all player scores.
+near call joker_poker.ximon.testnet get_all_players --accountId=ximon.testnet 
+
+# Interacting with the joker_poker game
+near call joker_poker.ximon.testnet joker_poker '{"guessed_card": {"rank": "Nine", "suit": "Diamond"}}' --accountId=ximon.testnet
+
+# Interacting with the black_red game
+near call joker_poker.ximon.testnet black_red '{"guessed_color": "Black"}' --accountId=ximon.testnet
