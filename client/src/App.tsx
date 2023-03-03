9a833beb36357331fdb18d5d5943fa3a2acf89b0
Simon Samuel <salviensky@gmail.com>
2023-03-03 22:28:25 +0100

    feat: migrated connect button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,15 +20,15 @@ function App() {
   // }, []);
   return (
     <div className="text-center">
-      <header className="bg-amber-500 max-h-fit px-2.5 py-2.5 relative">
+      <header className="bg-orange-web max-h-fit px-2.5 py-2.5 relative">
         <nav className="flex items-center justify-between">
           <img src={logo} className="w-64 h-20" alt="logo" />
           <ul className="px-4 m-0">
             <li className="inline-block mx-0 my-5 ">
-              <a className="text-slate-900 font-bold px-3.5" href="./pages/howToPlay.tsx">How To Play</a>
+              <a className="text-oxford-blue font-bold px-3.5" href="./pages/howToPlay.tsx">How To Play</a>
             </li>
             <li className="inline-block mx-0 my-5 ">
-              <a className="text-slate-900 font-bold px-3.5" href="./pages/about.tsx">About</a>
+              <a className="text-oxford-blue font-bold px-3.5" href="./pages/about.tsx">About</a>
             </li>
             <li className="inline-block mx-0 my-5 ">
               <ConnectWallet />
@@ -38,7 +38,7 @@ function App() {
       </header>
       <main className="flex items-center justify-evenly min-h-[80vh]">
         <div>
-          <h1 className="text-slate-900 mx-1.5 my-12 text-6xl font-bold">Welcome to Joker Poker!</h1>
+          <h1 className="text-oxford-blue mx-1.5 my-12 text-6xl font-bold">Welcome to Joker Poker!</h1>
           <h2 className="mx-10">What game do you want to play?</h2>
 
           <div className="flex items-center justify-around flex-wrap">
@@ -56,7 +56,7 @@ function App() {
           </div>
         </div>
       </main>
-      <footer className="text-slate-900 flex flex-col items-center justify-center font-bold max-h-[20vh] text-2xl">
+      <footer className="text-oxford-blue flex flex-col items-center justify-center font-bold max-h-[20vh] text-2xl">
         <p>Made with &#9829; by Simon Samuel</p>
       </footer>
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
+    colors: {
+      "oxford-blue": "#14213D",
+      "orange-web": "#FCA311",
+      platinum: "#E5E5E5",
+      black: "#000000",
+      white: "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [],
